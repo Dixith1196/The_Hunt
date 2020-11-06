d03069f8bad3c8138b5b7c2981de43e3c738a6b6
Deekshith Maram <s538365@nwmissouri.edu>
2020-11-06 02:23:45 -0600

    test port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config({ path: '.env' })
 LOG.info('Environment variables loaded into process.env.')
 
 // log port (Heroku issue)
-const port = process.env.PORT || 3004
+const port = process.env.PORT || 3000
 LOG.info(`Running on ${port}`)
 
 // Are we in production or development?
