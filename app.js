a8139ac400fdda3d1cb79ea08b9df507d3847b86
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-11-05 23:06:51 -0600

    logger and routes/router.js commit

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+// Module Dependencies
+
 const path = require("path")
 const express = require ("express")
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const LOG = require('./utils/logger.js')
 
+
+// specify desired view engine (EJS)
+
+// set the root view folder
+app.set('views', path.join(__dirname, 'views'))
+
+
+// log every call and pass it on for handling
+app.use((req, res, next) => {
+    LOG.debug(`${req.method} ${req.url}`)
+    next()
+  })
 
 app.get("/",(req,res)=>{
     res.send("Hello World!!")
@@ -9,4 +25,7 @@ app.get("/",(req,res)=>{
 
 app.listen(3000, () =>{
     console.log("The server is running on the port 3000")
+    console.log('Press CTRL-C to stop\n')
 })
+
+module.exports = app
