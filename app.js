1b7657a1ed132c150b0b3006f3d999e40dc87acd
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-11-05 19:34:28 -0600

    added app.js and script to start the app

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const path = require("path")
 const express = require ("express")
 const app = express()
 
+
 app.get("/",(req,res)=>{
-    res.send("Getting Started")
+    res.send("Hello World!!")
 })
 
 app.listen(3000, () =>{
