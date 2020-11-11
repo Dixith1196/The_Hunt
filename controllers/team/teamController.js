6b820489d58022517ae90435d3d49ba9e7907394
Vikas2004 <S538336@NWMISSOURI.EDU>
2020-11-11 16:16:28 -0600

    create and delete controller commit

--- controllers/team/teamController.js
+++ controllers/team/teamController.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const { Model } = require('mongoose')
 const api = express.Router()
 const TeamModel = require('../../models/team')
 const LOG = require('../../utils/logger')
 
 
-// Get all the data
+// GET all the data
 
 api.get('/findall', (req, res) => {
     LOG.info(`The function handling the /findall ${req}`)
@@ -17,7 +18,7 @@ api.get('/findall', (req, res) => {
 })
 
 
-// Get the data by id
+// GET the data by id
 
 api.get('/findone/:id', (req, res) => {
     LOG.info(`Handling /findone ${req}`)
@@ -25,5 +26,47 @@ api.get('/findone/:id', (req, res) => {
     TeamModel.find({ teamid: id }, (err, result) => {
         if (err) { return res.end(`Could not find the record for the teamid: ${id}`) }
         res.json(results[0])
+    })
+})
+
+
+// GET to this controller 
+
+api.get('/',(req,res)=>{
+    LOG.info(`Handling GET / ${req}`)
+    TeamModel.find({},(err,data)=>{
+        if (err) {
+            return res.end('error on create')
+        }
+        res.locals.teams = data
+        res.render('user/index.js')
+    })
+})
+
+// Respond with views
+
+// GET create
+
+api.get('/create',(req,res)=>{
+    LOG.info(`Handling GET /create ${req}`)
+    TeamModel.find({},(err,data)=>{
+        res.locals.teams =data
+        res.locals.team = new Model()
+        res.render('team/create')
+    })
+})
+
+// GET /delete/:id
+
+api.get('/delete/:id',(req, res)=>{
+    LOG.info(`Handling GET /delete/:id ${req}`)
+    const id = parseInt(req.params.id)
+    TeamModel.find({teamid:id},(err, results) =>{
+        if(err) {
+            return res.end(`Could not find the record to delete`)
+        }
+        LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+        res.locals.team = results[0]
+        return res.render(`team/delete.ejs`)
     })
 })
